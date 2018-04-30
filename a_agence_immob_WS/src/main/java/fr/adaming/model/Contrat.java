7845ed7ba59dbaa06805243662b3db0c833bd753
inti0333 <inti0333@DESKTOP-B6HP6U1>
2018-04-30 16:30:25 +0200

    mat : assos contrat

--- a_agence_immob_WS/src/main/java/fr/adaming/model/Contrat.java
+++ a_agence_immob_WS/src/main/java/fr/adaming/model/Contrat.java
@@ -2,45 +2,94 @@ package fr.adaming.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "contrats")
+@XmlRootElement
 public class Contrat {
-	
-	 private int id;
-	 private String ref;
-	 private Date dateSignature;
-	 
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_contrat")
+	private int id;
+	private String ref;
+	private Date dateSignature;
+
+	// Transfo assos avec client
+	@OneToOne
+	private Client client;
+
+	// Transfo assos avec bien immobilier
+	@OneToOne
+	private BienImmobilier bi;
+
+	// Getter et setter
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getRef() {
 		return ref;
 	}
+
 	public void setRef(String ref) {
 		this.ref = ref;
 	}
+
 	public Date getDateSignature() {
 		return dateSignature;
 	}
+
 	public void setDateSignature(Date dateSignature) {
 		this.dateSignature = dateSignature;
 	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public BienImmobilier getBi() {
+		return bi;
+	}
+
+	public void setBi(BienImmobilier bi) {
+		this.bi = bi;
+	}
+
+	// Constructeurs
 	public Contrat(int id, String ref, Date dateSignature) {
 		super();
 		this.id = id;
 		this.ref = ref;
 		this.dateSignature = dateSignature;
 	}
+
 	public Contrat(String ref, Date dateSignature) {
 		super();
 		this.ref = ref;
 		this.dateSignature = dateSignature;
 	}
+
 	public Contrat() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
+
 	@Override
 	public String toString() {
 		return "Contrat [id=" + id + ", ref=" + ref + ", dateSignature=" + dateSignature + "]";
