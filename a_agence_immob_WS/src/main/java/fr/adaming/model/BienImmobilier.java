24009697a37c027e85a9810c82aa7c7cf194e301
inti0323 <inti0323@DESKTOP-B2JC298>
2018-04-30 16:21:48 +0200

    Lise BI modif

--- a_agence_immob_WS/src/main/java/fr/adaming/model/BienImmobilier.java
+++ a_agence_immob_WS/src/main/java/fr/adaming/model/BienImmobilier.java
@@ -2,19 +2,33 @@ package fr.adaming.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
-@Table(name = "proprietaires")
+@Table(name = "bien_immobiliers")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="type")
 @XmlRootElement
 public class BienImmobilier {
 
 	// attributs
+	@Id
+	@Column(name="id_bi")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String statut;
 	@Temporal(TemporalType.DATE)
@@ -25,6 +39,14 @@ public class BienImmobilier {
 	private Date dateDispo;
 	private double revenuCadastral;
 	private double prix;
+	@Lob
+	private byte[] photoBI;
+	@Transient
+	private String imageBI;
+
+	
+	//transformations UML en java
+	
 
 	// constructeurs
 	public BienImmobilier() {
