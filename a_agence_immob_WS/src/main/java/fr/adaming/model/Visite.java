26200ad6a3248b03dff47fc48a144285cebadb67
inti0350 <inti0350@DESKTOP-B588GH8>
2018-04-30 16:01:22 +0200

    Wyndy : Visite

---
+++ a_agence_immob_WS/src/main/java/fr/adaming/model/Visite.java
@@ -0,0 +1,92 @@
+package fr.adaming.model;
+
+import java.io.Serializable;
+import java.sql.Time;
+import java.util.Date;
+import java.util.Timer;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name="visites")
+@XmlRootElement
+public class Visite implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Temporal(TemporalType.TIME)
+	private Date heure;
+	@Temporal(TemporalType.DATE)
+	private Date datevisite;
+	
+	
+	//Constructeurs
+	public Visite() {
+		super();
+	}
+
+
+	public Visite(Date heure, Date datevisite) {
+		super();
+		this.heure = heure;
+		this.datevisite = datevisite;
+	}
+
+
+	public Visite(int id, Date heure, Date datevisite) {
+		super();
+		this.id = id;
+		this.heure = heure;
+		this.datevisite = datevisite;
+	}
+
+	
+	//G+S
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public Date getHeure() {
+		return heure;
+	}
+
+
+	public void setHeure(Date heure) {
+		this.heure = heure;
+	}
+
+
+	public Date getDatevisite() {
+		return datevisite;
+	}
+
+
+	public void setDatevisite(Date datevisite) {
+		this.datevisite = datevisite;
+	}
+
+
+	//ToString
+	@Override
+	public String toString() {
+		return "Visite [id=" + id + ", heure=" + heure + ", datevisite=" + datevisite + "]";
+	}
+	
+	
+	
+	
+}
