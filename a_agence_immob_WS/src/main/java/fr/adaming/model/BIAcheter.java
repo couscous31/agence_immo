dcf7535c61bfe4923abc8fc961fc57a5e7626b69
inti0333 <inti0333@DESKTOP-B6HP6U1>
2018-05-02 09:37:30 +0200

    mat : bi louer et acheter

--- a_agence_immob_WS/src/main/java/fr/adaming/model/BIAcheter.java
+++ a_agence_immob_WS/src/main/java/fr/adaming/model/BIAcheter.java
@@ -4,45 +4,53 @@ import java.util.Date;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+
 @Entity
-@DiscriminatorValue(value="Acheter")
+@DiscriminatorValue(value = "Acheter")
 public class BIAcheter extends BienImmobilier {
-	
+
 	private double prixAchat;
 	private String etat;
+
+	// Constructeurs
+	public BIAcheter() {
+		super();
+	}
+
+	public BIAcheter(String statut, Date dateSoumission, Adresse adresse, Date dateDispo, double revenuCadastral,
+			double prix, double prixAchat, String etat) {
+		super(statut, dateSoumission, adresse, dateDispo, revenuCadastral, prix);
+		this.prixAchat = prixAchat;
+		this.etat = etat;
+	}
+
+	public BIAcheter(int id, String statut, Date dateSoumission, Adresse adresse, Date dateDispo,
+			double revenuCadastral, double prix, double prixAchat, String etat) {
+		super(id, statut, dateSoumission, adresse, dateDispo, revenuCadastral, prix);
+		this.prixAchat = prixAchat;
+		this.etat = etat;
+	}
+
+	// Getter et setter
 	public double getPrixAchat() {
 		return prixAchat;
 	}
+
 	public void setPrixAchat(double prixAchat) {
 		this.prixAchat = prixAchat;
 	}
+
 	public String getEtat() {
 		return etat;
 	}
+
 	public void setEtat(String etat) {
 		this.etat = etat;
 	}
-	public BIAcheter() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public BIAcheter(int id, String statut, Date dateSoumission, Adresse adresse, Date dateDispo,
-			double revenuCadastral, double prix) {
-		super(id, statut, dateSoumission, adresse, dateDispo, revenuCadastral, prix);
-		// TODO Auto-generated constructor stub
-	}
-	public BIAcheter(String statut, Date dateSoumission, Adresse adresse, Date dateDispo, double revenuCadastral,
-			double prix) {
-		super(statut, dateSoumission, adresse, dateDispo, revenuCadastral, prix);
-		// TODO Auto-generated constructor stub
-	}
+
 	@Override
 	public String toString() {
 		return "BIAcheter [prixAchat=" + prixAchat + ", etat=" + etat + "]";
 	}
-	
-
-	
-	
 
 }
--- a_agence_immob_WS/src/main/java/fr/adaming/model/BILouer.java
+++ a_agence_immob_WS/src/main/java/fr/adaming/model/BILouer.java
@@ -4,74 +4,87 @@ import java.util.Date;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+
 @Entity
-@DiscriminatorValue(value="Louer")
+@DiscriminatorValue(value = "Louer")
 public class BILouer extends BienImmobilier {
-	
+
 	private double caution;
 	private double loyer;
 	private double charges;
 	private String typeBail;
 	private boolean meubles;
+
+	// Constructeurs
+	public BILouer() {
+		super();
+	}
+
+	public BILouer(String statut, Date dateSoumission, Adresse adresse, Date dateDispo, double revenuCadastral,
+			double prix, double caution, double loyer, double charges, String typeBail, boolean meubles) {
+		super(statut, dateSoumission, adresse, dateDispo, revenuCadastral, prix);
+		this.caution = caution;
+		this.loyer = loyer;
+		this.charges = charges;
+		this.typeBail = typeBail;
+		this.meubles = meubles;
+	}
+
+	public BILouer(int id, String statut, Date dateSoumission, Adresse adresse, Date dateDispo, double revenuCadastral,
+			double prix, double caution, double loyer, double charges, String typeBail, boolean meubles) {
+		super(id, statut, dateSoumission, adresse, dateDispo, revenuCadastral, prix);
+		this.caution = caution;
+		this.loyer = loyer;
+		this.charges = charges;
+		this.typeBail = typeBail;
+		this.meubles = meubles;
+	}
+
+	// Getter et setter
 	public double getCaution() {
 		return caution;
 	}
+
 	public void setCaution(double caution) {
 		this.caution = caution;
 	}
+
 	public double getLoyer() {
 		return loyer;
 	}
+
 	public void setLoyer(double loyer) {
 		this.loyer = loyer;
 	}
+
 	public double getCharges() {
 		return charges;
 	}
+
 	public void setCharges(double charges) {
 		this.charges = charges;
 	}
+
 	public String getTypeBail() {
 		return typeBail;
 	}
+
 	public void setTypeBail(String typeBail) {
 		this.typeBail = typeBail;
 	}
+
 	public boolean isMeubles() {
 		return meubles;
 	}
+
 	public void setMeubles(boolean meubles) {
 		this.meubles = meubles;
 	}
-	public BILouer() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public BILouer(int id, String statut, Date dateSoumission, Adresse adresse, Date dateDispo, double revenuCadastral,
-			double prix) {
-		super(id, statut, dateSoumission, adresse, dateDispo, revenuCadastral, prix);
-		// TODO Auto-generated constructor stub
-	}
-	public BILouer(String statut, Date dateSoumission, Adresse adresse, Date dateDispo, double revenuCadastral,
-			double prix) {
-		super(statut, dateSoumission, adresse, dateDispo, revenuCadastral, prix);
-		// TODO Auto-generated constructor stub
-	}
-	public BILouer(double caution, double loyer, double charges, String typeBail, boolean meubles) {
-		super();
-		this.caution = caution;
-		this.loyer = loyer;
-		this.charges = charges;
-		this.typeBail = typeBail;
-		this.meubles = meubles;
-	}
+
 	@Override
 	public String toString() {
 		return "BILouer [caution=" + caution + ", loyer=" + loyer + ", charges=" + charges + ", typeBail=" + typeBail
 				+ ", meubles=" + meubles + "]";
 	}
-	
-	
-	
 
 }
