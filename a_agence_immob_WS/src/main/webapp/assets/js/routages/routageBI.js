326a4fb2034a9e80b8e87521402733fe42d85fc6
inti0350 <inti0350@DESKTOP-B588GH8>
2018-05-03 14:38:37 +0200

    Wyndy : BI immo

--- a_agence_immob_WS/src/main/webapp/assets/js/routages/routageBI.js
+++ a_agence_immob_WS/src/main/webapp/assets/js/routages/routageBI.js
@@ -5,6 +5,8 @@ monApp.config(function($routeProvider){
 			.when("/addBi",{controller:"ajoutBiCTRL", templateUrl: "vues/addBi.html"})
 			.when("/updateBi",{controller:"modifBiCTRL", templateUrl: "vues/modifBI.html"})
 			.when("/deleteBi",{controller:"supprBiCTRL", templateUrl: "vues/supprimBI.html"})
+			.when("/getById",{controller:"recBiCTRL", templateUrl: "vues/rechBI.html"})
+			.when("/getbyIdprop",{controller:"recBiPrCTRL", templateUrl: "vues/rechpropBI.html"})
 			.otherwise({
 				redirectTo:"/findAllBi"
 			})
